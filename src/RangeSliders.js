c7d873cdd754fe895da03e77f1ec9f202bd7ed68
Nader Zaman <naderzaman50@gmail.com>
2023-09-09 20:39:29 +0500

    Update RangeSliders.js

--- src/RangeSliders.js
+++ src/RangeSliders.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Slider } from '@mui/material'
 
 function RangeSliders() {
+    const [val, setVal] = useState([30,100]);
+    const updateVal = (e,val)=>{
+        setVal(val)
+    }
   return (
     <div style={{width:300 , margin:30}}>
-      <Slider/>
+      <Slider value={val} max={200} onChange={updateVal}/>
     </div>
   )
 }
